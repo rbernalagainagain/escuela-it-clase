c9aa93e9221cf358d908683b94884b2c4c8550d6
Rafael Bernal <immrb@MacBook-Pro-de-Rafael.local>
2020-07-04 18:28:39 +0200

    Install eslint && config && create gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    /* bundle rules */
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+        "strict": ["error", "never"],
+        "init-declarations": ["error", "always"]
+        /* add rules */
+    }
+};
